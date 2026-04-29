316856eebef10e3199cfcd2f47a8e0a8aefd77f7
QuocDat <datotis2103@gmail.com>
2026-04-30 02:20:07 +0700

    feat: update library app

---
+++ resort_app/lib/core/constants/dev_credentials.dart
@@ -0,0 +1,8 @@
+class DevCredentials {
+  // Để trống nếu không muốn tự động điền
+  static const String email = "datotis2103@gmail.com";
+  static const String password = "Adminqd@21";
+
+  // Set thành true để kích hoạt tự động điền
+  static const bool useDevCredentials = true;
+}
